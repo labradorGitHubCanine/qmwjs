c321c95f41363297dd1461985d2a6e686a21c7fd
labradorgithubcanine <123349527@qq.com>
2022-06-27 22:26:03 +0800

    0

--- util/number-util.js
+++ util/number-util.js
@@ -2,5 +2,8 @@ export default {
     isNumber(number) {
         let e = String(number).trim()
         return e !== '' && isFinite(+e) && !isNaN(+e)
+    },
+    ifNaN(number, substitute) {
+        return this.isNumber(number) ? +number : substitute
     }
 }
