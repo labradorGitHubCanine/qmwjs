34e1c73a324c60a797ab2339ed670fed6ef7fb8f
labradorgithubcanine <123349527@qq.com>
2022-08-23 22:37:34 +0800

    0

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import NumberUtil from './util/number-util'
 import JsonUtil from './util/json-util'
 import StorageUtil from './util/storage-util'
 import AxiosUtil from './util/axios-util'
+import UUID from './util/UUID'
 
 export {
     AxiosUtil,
     NumberUtil,
     JsonUtil,
     StorageUtil,
+    UUID,
 }
