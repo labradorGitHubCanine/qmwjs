c2f6d8c0159967aee57060dacbb71c92c9de99ae
qmw <123349527@qq.com>
2022-11-16 10:21:06 +0800

    0

--- util/axios-util.js
+++ util/axios-util.js
@@ -1,30 +1,26 @@
 /**
  * axios相关工具
  * qmw
- * 2022年7月19日, PM 05:16:35
+ * v1 2022年7月19日, PM 05:16:35
+ * v2 2022年11月16日, AM 10:20:55
  */
 export default {
     'serialize'(request) {
         if (request.headers['Content-Type'] !== 'multipart/form-data') {
+            const key = {
+                get: 'params',
+                delete: 'params',
+                post: 'data',
+                put: 'data',
+            }[request.method]
+
+            const data = request[key]
             const params = new URLSearchParams()
-
-        }
-
-
-
-
-
-
-        if (config.headers['Content-Type'] !== 'multipart/form-data') {
-            const params = new URLSearchParams()
-            let key = 'params'
-            if (['post', 'put'].includes(config.method)) key = 'data'
-            for (const k in config[key] || {}) {
-                const value = config[key][k]
-                if (![undefined, null, NaN, Infinity, -Infinity].includes(value))
-                    params.append(k, value)
+            for (const key in data || {}) {
+                const value = data[key]
+                params.append(key, value)
             }
-            config[key] = params
+            request[key] = params
         }
     }
 }
